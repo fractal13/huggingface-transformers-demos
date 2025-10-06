6c619e519fc961c3a2ce52bc677f9692008c56b1
Curtis Larsen <curtis.gale.larsen@gmail.com>
2025-10-06 10:00:05 -0600

    check for data files to give better diagnostics

--- src/image_caption_demo.py
+++ src/image_caption_demo.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 from demo_utils import get_best_device
 from transformers import pipeline
+import os
+
+if not os.path.exists("data/image.png"):
+    raise Exception("data/image.png does not exist.")
 
 device = get_best_device()
 image_to_text = pipeline("image-to-text", model="Salesforce/blip-image-captioning-base", device=device)
--- src/speech_recognition_demo.py
+++ src/speech_recognition_demo.py
@@ -2,6 +2,10 @@
 
 from demo_utils import get_best_device
 from transformers import pipeline
+import os
+
+if not os.path.exists("data/harvard.wav"):
+    raise Exception("data/harvard.wav does not exist.")
 
 device = get_best_device()
 speech_to_text = pipeline("automatic-speech-recognition", model="openai/whisper-small", device=device)
